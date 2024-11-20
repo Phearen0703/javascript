8cceba491a77273be8b687db366e63d7da90323b
PHEAREN <88501377+Phearen0703@users.noreply.github.com>
2024-11-20 15:31:31 +0700

    finish password

--- password vilidate/js/main.js
+++ password vilidate/js/main.js
@@ -1,24 +1,46 @@
 const form = document.querySelector("form");
 const pwdinput = document.querySelector("#password");
 
-form.addEventListener("keyup",()=>{
-   if(pwdinput.value.length >= 8){
-    const minChar = document.querySelector("#minChar");
+form.addEventListener("keyup", () => {
+  const minChar = document.querySelector("#minChar");
+  const LwCase = document.querySelector("#lwCase");
+  const UpCase = document.querySelector("#upCase");
+  const SpeChar = document.querySelector("#speChar");
+  const NumChar = document.querySelector("#numChar");
+
+  if (pwdinput.value.length >= 8) {
     minChar.style = "color : green";
-   }else{
+  } else {
     minChar.style = "color : black";
+  }
+
+
+  const LowercaseChar = /[a-z]/;
+  if (LowercaseChar.test(pwdinput.value)) {
+    LwCase.style = "color : green";
+  } else {
+    LwCase.style = "color : black";
+  }
+
+  const UperCaseChar = /[A-Z]/;
+  if (UperCaseChar.test(pwdinput.value)) {
+    UpCase.style = "color : green";
+  } else {
+    UpCase.style = "color : black";
+  }
+  
+  const specialChar = /[!@#$%^&*(),.?":{}|<>]/;
+  if(specialChar.test(pwdinput.value)){
+    SpeChar.style = "color : green";
+  }else{
+    SpeChar.style= "color : black";
+  }
+
+   const numberChar = /[0-9]/;
+   if(numberChar.test(pwdinput.value)){
+      NumChar.style = "color : green";
+   }else{
+      NumChar.style = "color : black";
    }
 
-   const LowercaseChar =  /^[a-z]+$/;
-   if(LowercaseChar.test(pwdinput.value)){
-    const LwCase = document.querySelector("#lwCase");
-        LwCase.style="color : green";
-   }
-
-   const UperCaseChar = /^[A-Z]+$/;
-   if(UperCaseChar.test(pwdinput.value)){
-      const UpCase = document.querySelector("#upCase");
-         UpCase.style="color : green";
-   }
-
-})
+});
